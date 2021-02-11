0b8901b83b972552cec8662147ff356fa194d0f0
Oliver Cooper <oliver.d.cooper@gmail.com>
2021-02-11 19:01:54 +0000

    Fix DataFilter implementation

--- src/AbpQueryFilterDemo.Domain/Extensions/DataFilter/CustomDataFilter.cs
+++ src/AbpQueryFilterDemo.Domain/Extensions/DataFilter/CustomDataFilter.cs
@@ -39,36 +39,133 @@ namespace AbpQueryFilterDemo
     public interface IDataFilter : ISingletonDependency
     {
         /// <summary>
-        /// The filters that are currently active.
+        /// The filters that have been cached. These can be filtered by <see cref="IBasicDataFilter.IsActive"/> to find the filters that are in active use.
+        /// <para>
+        ///     This is for reference only and should not be relied upon. Please use <see cref="IsActive{TFilter}"/> or <see cref="IsEnabled{TFilter}"/> for accurate results.
+        /// </para>
         /// </summary>
         IReadOnlyDictionary<Type, IBasicDataFilter> ReadOnlyFilters { get; }
-        /// <summary>
-        /// The current <see cref="AbpDataFilterOptions.DefaultStates"/>.
-        /// </summary>
-        IReadOnlyDictionary<Type, DataFilterState> DefaultFilterStates { get; }
 
+        /// <summary>
+        /// <para>
+        ///     Enables the given filter. This should be disposed of after use to reset the filter to its previous state!
+        /// </para>
+        /// <example>
+        ///     <b>Example:</b> <c>using(DataFilter.Enable&lt;ISoftDelete&gt;) { /* code here */ }</c>
+        /// </example>
+        /// </summary>
+        /// <typeparam name="TFilter">The filter to enable. </typeparam>
+        /// <returns>A disposable which resets the filter after use. </returns>
         IDisposable Enable<TFilter>() where TFilter : class;
 
+        /// <summary>
+        /// <para>
+        ///     Disables the given filter. This should be disposed of after use to reset the filter to its previous state!
+        /// </para>
+        /// <example>
+        ///     <b>Example:</b> <c>using(DataFilter.Disable&lt;ISoftDelete&gt;) { /* code here */ }</c>
+        /// </example>
+        /// </summary>
+        /// <typeparam name="TFilter">The filter to disable. </typeparam>
+        /// <returns>A disposable which resets the filter after use. </returns>
         IDisposable Disable<TFilter>() where TFilter : class;
 
+        /// <summary>
+        /// <para>
+        ///     Determines if the <see cref="IDataFilter{TFilter}"/> is active for the given <typeparamref name="TFilter"/>.
+        ///     <br/>A filter is active when <see cref="Enable{TFilter}"/> or <see cref="Disable{TFilter}"/> has been called and is in active use.
+        ///     <br/>When the filter is disposed, it will be set to inactive again.
+        /// </para>
+        /// <example>
+        ///     Example usage: <see cref="DataFilter.IsActive{TFilter}"/>
+        /// </example>
+        /// </summary>
+        /// <typeparam name="TFilter">The <see cref="Type"/> of filter to find i.e. <see cref="ISoftDelete"/>. </typeparam>
+        /// <returns>
+        ///     <see langword="true"/> if the filter is active for the given <typeparamref name="TFilter"/>, 
+        ///     otherwise <see langword="false"/>.
+        /// </returns>
         bool IsActive<TFilter>() where TFilter : class;
 
+        /// <summary>
+        /// <para>
+        ///     Determines if the <see cref="IDataFilter{TFilter}"/> is active for the given <paramref name="filterType"/>.
+        ///     <br/>A filter is active when <see cref="Enable{TFilter}"/> or <see cref="Disable{TFilter}"/> has been called and is in active use.
+        ///     <br/>When the filter is disposed, it will be set to inactive again.
+        /// </para>
+        /// <para>
+        ///     <b>Note</b>: This method should only be used when the <see cref="IDataFilter{TFilter}"/> type is not known until runtime as there is a performance penalty for using it.
+        ///     <br/>Please use <see cref="IsActive{TFilter}"/> if the filter type is known at compile-time e.g. when using the generic <see cref="ISoftDelete"/>.
+        /// </para>
+        /// <example>
+        ///     Example usage: <c>DataFilter.IsActive(typeof(myDynamicFilter))</c>
+        /// </example>
+        /// </summary>
+        /// <param name="filterType">The <see cref="Type"/> of filter to find i.e. <see cref="ISoftDelete{TEntity}"/>. </param>
+        /// <returns>
+        ///     <see langword="true"/> if the filter is active for the given <paramref name="filterType"/>, 
+        ///     otherwise <see langword="false"/>.
+        /// </returns>
         bool IsActive(Type filterType);
 
-        bool IsEnabled<TFilter>(bool cacheResult = true) where TFilter : class;
+        /// <summary>
+        /// <para>
+        ///     Determines if the <see cref="IDataFilter{TFilter}"/> is enabled for the given <typeparamref name="TFilter"/>.
+        /// </para>
+        /// <example>
+        ///     Example usage: <see cref="DataFilter.IsEnabled{TFilter}"/>
+        /// </example>
+        /// </summary>
+        /// <typeparam name="TFilter">The <see cref="Type"/> of filter to find i.e. <see cref="ISoftDelete"/>. </typeparam>
+        /// <returns>
+        ///     <see langword="true"/> if the filter is enabled for the given <paramref name="filterType"/>, 
+        ///     otherwise the default value for this filter defined in the <see cref="AbpDataFilterOptions.DefaultStates"/> (normally <see langword="true"/>). 
+        /// </returns>
+        bool IsEnabled<TFilter>() where TFilter : class;
 
-        bool IsEnabled(Type filterType, bool cacheResult = true);
+        /// <summary>
+        /// Determines if the <see cref="IDataFilter{TFilter}"/> is enabled for the given <paramref name="filterType"/>.
+        /// <para>
+        ///     <b>Note</b>: This method should only be used when the <see cref="IDataFilter{TFilter}"/> type is not known until runtime as there is a performance penalty for using it.
+        ///     <br/>Please use <see cref="IsEnabled{TFilter}"/> if the filter type is known at compile-time e.g. when using the generic <see cref="ISoftDelete"/>.
+        /// </para>
+        /// <example>
+        ///     Example usage: <c>DataFilter.IsEnabled(typeof(myDynamicFilter))</c>
+        /// </example>
+        /// </summary>
+        /// <param name="filterType">The <see cref="Type"/> of filter to find i.e. <see cref="ISoftDelete{TEntity}"/>. </param>
+        /// <returns>
+        ///     <see langword="true"/> if the filter is enabled for the given <paramref name="filterType"/>, 
+        ///     otherwise the default value for this filter defined in the <see cref="AbpDataFilterOptions.DefaultStates"/> (normally <see langword="true"/>). 
+        /// </returns>
+        bool IsEnabled(Type filterType);
 
-        IDataFilter<TFilter> GetFilter<TFilter>(bool cacheResult = true) where TFilter : class;
+        /// <summary>
+        /// Gets the <see cref="IDataFilter{TFilter}"/> representing the <typeparamref name="TFilter"/>.
+        /// If the filter doesn't exist then a new instance will be created.
+        /// </summary>
+        /// <typeparam name="TFilter">The type of filter e.g. <see cref="ISoftDelete"/>. </typeparam>
+        IDataFilter<TFilter> GetOrAddFilter<TFilter>() where TFilter : class;
 
-        IBasicDataFilter GetFilter(Type filterType, bool cacheResult = true);
+        /// <summary>
+        /// Gets the <see cref="IBasicDataFilter"/> representing the <paramref name="filterType"/>.
+        /// If the filter doesn't exist then a new instance will be created.
+        /// <para>
+        ///     <b>Note</b>: This method should only be used when the <see cref="IDataFilter{TFilter}"/> type is not known until runtime as there is a performance penalty for using it.
+        ///     <br/>Please use <see cref="GetOrAddFilter{TFilter}"/> if the filter type is known at compile-time e.g. when using the generic <see cref="ISoftDelete"/>.
+        /// </para>
+        /// <para>
+        ///     It should be possible to cast the <see cref="IBasicDataFilter"/> to your dynamic type.
+        ///     <br/>Example: <c>(myDynamicFilter as IDataFilter&lt;ISoftDelete&lt;MyClass&gt;&gt;)</c>
+        /// </para>
+        /// </summary>
+        /// <param name="filterType">The type of filter e.g. <c>typeof(myDynamicFilter)</c>. </param>
+        IBasicDataFilter GetOrAddFilter(Type filterType);
     }
 
     public class DataFilter : IDataFilter
     {
         public IReadOnlyDictionary<Type, IBasicDataFilter> ReadOnlyFilters => Filters;
-
-        public IReadOnlyDictionary<Type, DataFilterState> DefaultFilterStates { get; } = new Dictionary<Type, DataFilterState>();
 
         protected readonly ConcurrentDictionary<Type, IBasicDataFilter> Filters = new();
 
@@ -81,67 +178,50 @@ namespace AbpQueryFilterDemo
             ServiceProvider = serviceProvider;
         }
 
-        public virtual IDisposable Enable<TFilter>() where TFilter : class
+        public virtual IDisposable Enable<TFilter>()
+            where TFilter : class
         {
-            return GetFilter<TFilter>().Enable();
+            return GetOrAddFilter<TFilter>().Enable();
         }
 
-        public virtual IDisposable Disable<TFilter>() where TFilter : class
+        public virtual IDisposable Disable<TFilter>()
+            where TFilter : class
         {
-            return GetFilter<TFilter>().Disable();
+            return GetOrAddFilter<TFilter>().Disable();
         }
 
-        public virtual bool IsActive<TFilter>() where TFilter : class
+        public virtual bool IsActive<TFilter>()
+            where TFilter : class
         {
-            return GetFilter<TFilter>(false)?.IsActive ?? false;
+            return GetOrAddFilter<TFilter>().IsActive;
         }
 
         public virtual bool IsActive(Type filterType)
         {
-            return GetFilter(filterType, false)?.IsActive ?? false;
+            return GetOrAddFilter(filterType).IsActive;
         }
 
-        public virtual bool IsEnabled<TFilter>(bool cacheResult = true)
+        public virtual bool IsEnabled<TFilter>()
             where TFilter : class
         {
-            return GetFilter<TFilter>(cacheResult).IsEnabled;
+            return GetOrAddFilter<TFilter>().IsEnabled;
         }
 
-        public virtual bool IsEnabled(Type filterType, bool cacheResult = true)
+        public virtual bool IsEnabled(Type filterType)
         {
-            var foundFilter = GetFilter(filterType, cacheResult);
-            if (foundFilter != null)
-            {
-                return foundFilter.IsEnabled;
-            }
-
-            return DefaultFilterStates.GetOrDefault(filterType)?.IsEnabled ?? true;
+            return GetOrAddFilter(filterType).IsEnabled;
         }
 
-        public virtual IDataFilter<TFilter> GetFilter<TFilter>(bool cacheResult = true)
+        public virtual IDataFilter<TFilter> GetOrAddFilter<TFilter>()
             where TFilter : class
         {
-            if (cacheResult)
-            {
-                return Filters.GetOrAdd(
-                    typeof(TFilter),
-                    () => ServiceProvider.GetRequiredService<IDataFilter<TFilter>>()
-                ) as IDataFilter<TFilter>;
-            }
-            else
-            {
-                // note: not using GetFilter(Type...) because this will be more performant
-
-                if (Filters.TryGetValue(typeof(TFilter), out var value))
-                {
-                    return (IDataFilter<TFilter>)value;
-                }
-
-                return ServiceProvider.GetRequiredService<IDataFilter<TFilter>>();
-            }
+            return Filters.GetOrAdd(
+                typeof(TFilter),
+                () => ServiceProvider.GetRequiredService<IDataFilter<TFilter>>()
+            ) as IDataFilter<TFilter>;
         }
 
-        public virtual IBasicDataFilter GetFilter(Type filterType, bool cacheResult = true)
+        public virtual IBasicDataFilter GetOrAddFilter(Type filterType)
         {
             if (filterType == null
                 // Should have no more than 1 interface type argument
@@ -154,24 +234,11 @@ namespace AbpQueryFilterDemo
                 throw new AbpException($"The {nameof(filterType)} '{(filterType == null ? "<null>" : filterType.Name)}' is not a valid data filter type");
             }
 
-            if (cacheResult)
-            {
-                return Filters.GetOrAdd(
-                    filterType,
-                    (type) => ServiceProvider.GetRequiredService(
-                        typeof(IDataFilter<>).MakeGenericType(type)) as IBasicDataFilter
-                );
-            }
-            else
-            {
-                if (Filters.TryGetValue(filterType, out var value))
-                {
-                    return value;
-                }
-
-                return ServiceProvider.GetRequiredService(
-                    typeof(IDataFilter<>).MakeGenericType(filterType)) as IBasicDataFilter;
-            }
+            return Filters.GetOrAdd(
+                filterType,
+                (type) => ServiceProvider.GetRequiredService(
+                    typeof(IDataFilter<>).MakeGenericType(type)) as IBasicDataFilter
+            );
         }
     }
 
@@ -202,42 +269,52 @@ namespace AbpQueryFilterDemo
         {
             EnsureInitialized();
 
-            Filter.Value.IsActive = true;
-
-            if (IsEnabled)
+            if (!IsEnabled)
             {
-                return new DisposeAction(() => {
-                    Filter.Value.IsActive = false;
-                });
+                Filter.Value.IsEnabled = true;
             }
 
-            Filter.Value.IsEnabled = true;
+            if (IsActive)
+            {
+                return new DisposeAction(() => {
+                    if (IsEnabled) Filter.Value.IsEnabled = false;
+                });
+            }
+            else
+            {
+                Filter.Value.IsActive = true;
 
-            return new DisposeAction(() => {
-                Filter.Value.IsActive = false;
-                Filter.Value.IsEnabled = false;
-            });
+                return new DisposeAction(() => {
+                    Filter.Value.IsActive = false;
+                    if (IsEnabled) Filter.Value.IsEnabled = false;
+                });
+            }
         }
 
         public virtual IDisposable Disable()
         {
             EnsureInitialized();
 
-            Filter.Value.IsActive = true;
-
-            if (!IsEnabled)
+            if (IsEnabled)
             {
-                return new DisposeAction(() => {
-                    Filter.Value.IsActive = false;
-                });
+                Filter.Value.IsEnabled = false;
             }
 
-            Filter.Value.IsEnabled = false;
+            if (IsActive)
+            {
+                return new DisposeAction(() => {
+                    if (!IsEnabled) Filter.Value.IsEnabled = true;
+                });
+            }
+            else
+            {
+                Filter.Value.IsActive = true;
 
-            return new DisposeAction(() => {
-                Filter.Value.IsActive = false;
-                Filter.Value.IsEnabled = true;
-            });
+                return new DisposeAction(() => {
+                    Filter.Value.IsActive = false;
+                    if (!IsEnabled) Filter.Value.IsEnabled = true;
+                });
+            }
         }
 
         protected virtual void EnsureInitialized()
